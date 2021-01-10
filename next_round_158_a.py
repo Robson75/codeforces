0055e2a9ec4ec2e3f11647624347d2bcc258c8e0
Robson75 <rsamuelsson@gmail.com>
2021-01-10 18:41:02 +0100

    Change variable name

--- next_round_158_a.py
+++ next_round_158_a.py
@@ -5,11 +5,10 @@ class NextRound:
 
 if __name__ == "__main__":
     (n, k) = map(int, input().split())
-    total = 0
-    scores = []
+    to_next_round = 0
     scores = list(map(int, input().split()))
     k_score = scores[k - 1]
     for score in scores:
         if score > 0 and score >= k_score:
-            total += 1
-    print(total)
+            to_next_round += 1
+    print(to_next_round)
