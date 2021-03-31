69d9b1b777baeaf0b3315a19276973a16919b294
Robson75 <rsamuelsson@gmail.com>
2021-03-31 19:43:18 +0200

    Add design_tutorial_learn_from_math_472_a.py

---
+++ 800/design_tutorial_learn_from_math_472_a.py
@@ -0,0 +1,11 @@
+class DesignTutorialLearnFromMath:
+
+    pass
+
+
+if __name__ == "__main__":
+    n = int(input())
+    if n % 2 == 0:
+        print('4 ' + str(n - 4))
+    else:
+        print('9 ' + str(n - 9))
